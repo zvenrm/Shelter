59c21ae9d04d4ac6fd9b23038ac6ce8e7e0c5da3
zvenrm <zvennnnnn@gmail.com>
2022-04-22 01:47:32 +0300

    feat: create burger menu for our pets

---
+++ pages/pets/script.js
@@ -0,0 +1,22 @@
+const burger = document.querySelector('.header-burger')
+const burgerMenu = document.querySelector('.nav')
+const logo = document.querySelector('.logo')
+const overlay = document.querySelector('.overlay')
+
+
+function toggleMenu() {
+    burgerMenu.classList.toggle('nav-open')
+    logo.classList.toggle('logo-open')
+    document.body.classList.toggle('body-hidden')
+    overlay.classList.toggle('overlay-block')
+    burger.classList.toggle('burger-open')
+}
+
+burger.addEventListener('click', toggleMenu)
+burgerMenu.addEventListener('click', (e) => {
+    if (e.target.classList.contains('nav-link')){
+        toggleMenu()
+    }
+})
+overlay.addEventListener('click', toggleMenu)
+logo.addEventListener('click', toggleMenu)
